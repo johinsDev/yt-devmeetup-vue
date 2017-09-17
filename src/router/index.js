d01e04b89ac412e17911d77abd3514f9e57cbd34
JohinsDev <johinsdev@gmail.com>
2017-09-16 22:56:59 -0500

    Create dynamic menu

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Meetups from '@/components/Meetup/'
+import CreateMeetup from '@/components/Meetup/Create'
+import Profile from '@/components/User/Profile'
+import Signup from '@/components/auth/SignUp'
+import Signin from '@/components/Auth/SignIn'
 
 Vue.use(Router)
 
@@ -8,8 +13,34 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/meetups',
+      name: 'Meetups',
+      component: Meetups
+    },
+    {
+      path: '/meetup/new',
+      name: 'CreateMeetup',
+      component: CreateMeetup
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/sign-up',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/sign-in',
+      name: 'Signin',
+      component: Signin
     }
-  ]
+  ],
+  mode: 'history'
 })
